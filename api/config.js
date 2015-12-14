e8653643e96b6f09364efc6b80926d4d47afac59
Pedro Januário <prnjanuario@gmail.com>
2015-12-14 17:02:34 +0100

    added service configuration info

--- api/config.js
+++ api/config.js
@@ -1,5 +1,15 @@
 var config = {
-  port: 8081
+  port: 8081,
+  service: {
+    // broker frontend address
+    broker: 'tcp://127.0.0.1:7777',
+    // service unique identifier
+    sid: 'TASKS',
+    // client identity (optional), defaults to 'client'
+    identity: "API",
+    // client timeout in ms (optional), defaults to 1s
+    timeout: 1000
+  }
 };
 
 module.exports = config;
