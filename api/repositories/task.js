f1249d138032770f2556f92df43d8baac6449143
Pedro Januário <prnjanuario@gmail.com>
2015-12-14 17:02:08 +0100

    added task repository to fetch data from service instance

---
+++ api/repositories/task.js
@@ -0,0 +1,25 @@
+var ZSSClient = require('zmq-service-suite-client'),
+    config = require('../config'),
+    Logger = require('../logger'),
+    log = Logger.getLogger("TaskRepository"),
+    Task = require('../models/task');
+
+var client = new ZSSClient(config.service);
+
+var repository = {
+  get: function(id, callback) {
+    var payload = { id: id };
+    client.call("get", payload)
+      .then(function(payload){
+        var data = new Task(payload);
+        callback(null, data);
+      })
+      .fail(function(error){
+        log.info("fail with code: %s and user message: %s and dev message %s",
+          error.code, error.userMessage, error.developerMessage);
+        callback(error, null);
+      });
+  }
+};
+
+module.exports = repository;
