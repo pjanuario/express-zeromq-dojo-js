bb7cea3cb5120566f356f935901bea4cc437ad74
Pedro Januário <prnjanuario@gmail.com>
2015-12-18 14:35:47 +0000

    added user repo skeleton

--- service/repositories/user.js
+++ service/repositories/user.js
@@ -1,20 +1,24 @@
 var request = require('superagent'),
     config = require('../config'),
+    Logger = require('../logger'),
+    log = Logger.getLogger("UserRepository"),
     util = require('util');
 
 var repo = {
   get: function(id, callback){
     var url = util.format("%s/users/%s", config.get('github').uri, id);
+    log.info("Request made to %s", url);
     request
       .get(url)
       .end(function(err, res){
-        if (!err && !res.ok) {
-          err = err || new Error("something wrong");
-          callback(err, null);
-        } else {
-          var data = JSON.stringify(res.body);
-          callback(null, data);
+        var payload = res.body;
+        log.info("Response received from %s with status code: %s and payload %j", url, res.status, payload);
+
+        if (err || !res.ok) {
+          return callback(err || new Error("something wrong"), null);
         }
+
+        callback(null, payload);
       });
   }
 };
